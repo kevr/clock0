2f37224800adadec74ac2d28c57f43cad6545287
Kevin Morris <kevr@0cost.org>
2023-06-13 13:17:28 -0700

    fix: remove redundant #include
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#include "ncurses.hpp"
 #include "tui/tui.hpp"
 using namespace clock0;
 
